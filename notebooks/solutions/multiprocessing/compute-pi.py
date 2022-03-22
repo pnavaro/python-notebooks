871961aa6afcd926ce40c286cb4358d21be9c16a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-03-22 09:47:35 +0100

    Update compute-pi.py

--- notebooks/solutions/multiprocessing/compute-pi.py
+++ notebooks/solutions/multiprocessing/compute-pi.py
@@ -7,20 +7,23 @@ def compute_pi(n):
     for i in range(n):
         x=random.random()
         y=random.random()
-        if x*x + y*y <= 1: count+=1
+        if x*x + y*y <= 1:
+            count += 1
     return count
 
 times = []
-for np in range(1,cpu_count()+1):
+for nproc in (1, 2, 4, 8, 16):
     elapsed_time = time.time()
-    n = 4 * 10**7
-    part_count=[n//np] * np
-    with ProcessPoolExecutor(np) as pool: 
-        count=pool.map(compute_pi, part_count)
-    pi = 4*sum(count)/n
-    print ("Number of cores {0}, Error : {1:.8f}"
-       " time : {2:.8f}".format(np, abs(pi-math.pi) ,time.time()-elapsed_time))
-    times.append(time.time()-elapsed_time)
+    samples = 100000000
+    part_count = [samples // nproc] * nproc
+    if __name__ == '__main__':
+        with ProcessPoolExecutor(nproc) as pool:
+            count=pool.map(compute_pi, part_count)
+
+        pi = 4*sum(count)/samples
+        times.append(time.time()-elapsed_time)
+        print ("Number of cores {0}, Error : {1:.8f}"
+           " time : {2:.8f}".format(nproc, abs(pi-math.pi) ,time.time()-elapsed_time))
 
 
 # plot the speed-up of your solution ...
@@ -29,4 +32,5 @@ import seaborn as sns
 sns.set()
 procs = [p+1 for p in range(len(times))]
 etimes = [times[0]/t for t,p in zip(times,procs)]
-plt.plot(procs,etimes,'b-o', procs, procs, 'r-');
+plt.plot(procs,etimes,'b-o', procs, procs, 'r-')
+
