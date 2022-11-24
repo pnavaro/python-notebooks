9c5a00ad772a418199063cca36434540712e0ef8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-11-24 10:35:15 +0100

    improve laplace example

--- notebooks/solutions/numpy/laplace.py
+++ notebooks/solutions/numpy/laplace.py
@@ -8,19 +8,20 @@ Tnorth, Tsouth, Twest, Teast = 100, 20, 50, 50
 # Set meshgrid
 n, l = 64, 1.0
 X, Y = np.meshgrid(np.linspace(0,l,n), np.linspace(0,l,n))
-T = np.zeros((n,n))
+T = np.ones((n,n))
 
 # Set Boundary condition
-T[n-1:, :] = Tnorth
-T[:1, :] = Tsouth
-T[:, n-1:] = Teast
-T[:, :1] = Twest
+T[-1, :] = Tnorth
+T[0, :] = Tsouth
+T[:, -1] = Teast
+T[:, 0] = Twest
 
 for istep in itertools.count():
-    T_old = T[1:-1,1:-1]
-    T_new = (T[1:-1,2:]+T[2:,1:-1]+T[1:-1,:-2]+T[:-2,1:-1])*0.25
-    if np.allclose(T_new, T_old, rtol=1e-5): break
-    T[1:-1,1:-1] = T_new
+    T_old = T.copy()
+    T[1:-1,1:-1] = (T[1:-1,2:]+T[2:,1:-1]+T[1:-1,:-2]+T[:-2,1:-1])*0.25
+    residual = np.max(np.abs((T_old - T)/T_old))
+    print ((istep, residual), end="\r")
+    if residual < 1e-5: break
 
 print()
 print("iterations = ",istep)
--- notebooks/solutions/numpy/laplace_with_loops.py
+++ notebooks/solutions/numpy/laplace_with_loops.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 # Boundary conditions
 Tnorth, Tsouth, Twest, Teast = 100, 20, 50, 50
 
@@ -7,23 +9,23 @@ X, Y = np.meshgrid(np.linspace(0,l,n), np.linspace(0,l,n))
 T = np.zeros((n,n))
 
 # Set Boundary condition
-T[n-1:, :] = Tnorth
-T[:1, :] = Tsouth
-T[:, n-1:] = Teast
-T[:, :1] = Twest
+T[-1, :] = Tnorth
+T[0, :] = Tsouth
+T[:,-1:] = Teast
+T[:,0] = Twest
 
-residual = 1.0   
+
 istep = 0
+residual = 1.0
 while residual > 1e-5 :
     istep += 1
     print ((istep, residual), end="\r")
-    residual = 0.0   
+    T_old = np.copy(T)
     for i in range(1, n-1):
         for j in range(1, n-1):
-            T_old = T[i,j]
             T[i, j] = 0.25 * (T[i+1,j] + T[i-1,j] + T[i,j+1] + T[i,j-1])
-            if T[i,j]>0:
-                residual=max(residual,abs((T_old-T[i,j])/T[i,j]))
+
+    residual=np.max(np.abs((T_old-T)/T))
 
 
 print()
