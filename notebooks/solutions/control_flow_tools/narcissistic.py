cb1bb28b3cd5dbf0e887103f977deb838242341a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-06-25 16:41:05 +0200

    minor fix before ensai session

--- notebooks/solutions/control_flow_tools/narcissistic.py
+++ notebooks/solutions/control_flow_tools/narcissistic.py
@@ -1,17 +1,14 @@
 def narcissitic(n):
     """
-    Return True if n is a narcissitic number with 3 digits """
-    assert len(str(n)) == 3  # check if n contains 3 digits
+    Return True if n is a narcissitic number """
+
+    n_d = len(str(n))
     s = 0
     tmp = n
     while tmp > 0:
         d = tmp % 10
-        s += d ** 3
+        s += d ** n_d
         tmp //= 10
-    if s == n:
-        return True
-    else:
-        return False
+    return s == n
 
-
-print([n for n in range(100, 1000) if narcissitic(n)])
+print([n for n in range(1000, 10000) if narcissitic(n)])
